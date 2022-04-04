548fc1c6ddf32b750c3acd3522c522cc19861ed6
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-04-04 17:24:07 +0600

    404 not found route added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Blogs from './components/Blogs/Blogs';
 import DashBoard from './components/DashBoard/DashBoard';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 import Reviews from './components/Reviews/Reviews';
 
 function App() {
@@ -15,8 +17,8 @@ function App() {
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path='/dashboard' element={<DashBoard></DashBoard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route path='/about'></Route>
-        <Route path='*'></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='flex items-center justify-center mt-40'>
+            <div>
+                <h1 className='text-3xl font-semibold'>Sorry, This Page Not Found</h1>
+                <h1 className='text-6xl text-red-800'>404</h1>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
